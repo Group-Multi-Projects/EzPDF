012fb5402b2740ea4d6dc83f7b1b8008bf9eb5f5
anluu203 <151427161+anluu203@users.noreply.github.com>
2024-08-07 23:45:42 +0700

    add some file

---
+++ Front-end/assets/js/index.js
@@ -0,0 +1,9 @@
+function openNav() {
+    $("#mySidepanel").css("width", "90%");
+    $("#overlay").fadeIn();
+}
+
+function closeNav() {
+    $("#mySidepanel").css("width", "0");
+    $("#overlay").fadeOut();
+}
