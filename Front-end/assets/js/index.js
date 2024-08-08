9ca9d7ff547668486d34cdbed0371b019b8e6364
anluu203 <151427161+anluu203@users.noreply.github.com>
2024-08-08 22:32:21 +0700

    update

--- Front-end/assets/js/index.js
+++ Front-end/assets/js/index.js
@@ -1,5 +1,10 @@
+// SIGNUP
 function openNav() {
-    $("#mySidepanel").css("width", "90%");
+    if ($(window).width() < 971) {
+        $("#mySidepanel").css("width", "100%");
+    } else {
+        $("#mySidepanel").css("width", "90%");
+    }
     $("#overlay").fadeIn();
 }
 
@@ -7,3 +12,20 @@ function closeNav() {
     $("#mySidepanel").css("width", "0");
     $("#overlay").fadeOut();
 }
+
+// LOGIN
+function openNav2() {
+    if ($(window).width() < 971) {
+        $("#mySidepanel2").css("width", "100%");
+    } else {
+        $("#mySidepanel2").css("width", "90%");
+    }
+    $("#overlay").fadeIn();
+}
+
+function closeNav2() {
+    $("#mySidepanel2").css("width", "0");
+    $("#overlay").fadeOut();
+}
+
+
