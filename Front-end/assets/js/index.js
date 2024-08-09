0ca3d116a6474a9349145a546c1c80429672ce73
anluu203 <151427161+anluu203@users.noreply.github.com>
2024-08-09 23:17:41 +0700

    add login, signup

--- Front-end/assets/js/index.js
+++ Front-end/assets/js/index.js
@@ -1,31 +1,32 @@
 // SIGNUP
 function openNav() {
-    if ($(window).width() < 971) {
-        $("#mySidepanel").css("width", "100%");
+    if ($(window).width() < 992) {
+        $("#my_signup").css("width", "100%");
     } else {
-        $("#mySidepanel").css("width", "90%");
+        $("#my_signup").css("width", "90%");
     }
     $("#overlay").fadeIn();
 }
 
 function closeNav() {
-    $("#mySidepanel").css("width", "0");
+    $("#my_signup").css("width", "0");
     $("#overlay").fadeOut();
 }
 
 // LOGIN
 function openNav2() {
-    if ($(window).width() < 971) {
-        $("#mySidepanel2").css("width", "100%");
+    if ($(window).width() < 992) {
+        $("#my_login").css("width", "100%");
     } else {
-        $("#mySidepanel2").css("width", "90%");
+        $("#my_login").css("width", "90%");
     }
     $("#overlay").fadeIn();
 }
 
 function closeNav2() {
-    $("#mySidepanel2").css("width", "0");
+    $("#my_login").css("width", "0");
     $("#overlay").fadeOut();
 }
+
 
 
