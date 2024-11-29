8d5c5d52977f5113d379ee0d4c0574f3322b1ed3
Nguyễn Đình Thái <dinhthai0312@gmail.com>
2024-11-29 20:46:57 +0700

    Add cors

--- EzPDF/settings.py
+++ EzPDF/settings.py
@@ -220,6 +220,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8000',
     'http://127.0.0.1:8000',
+    'http://54.251.1.21:80/'
 ]
 CORS_ALLOW_METHODS = [
     'GET',
